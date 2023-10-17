6e273da86b849fe1e88e1b9f924124653c2853de
Paola Sensato Pasti <paolasensato03@gmail.com>
2023-10-17 20:32:33 -0300

    Refatora home, criando componente de lista

---
+++ src/components/ListPets.tsx
@@ -0,0 +1,135 @@
+import React from 'react';
+import {FlatList, StyleSheet, View, Alert} from 'react-native';
+import {Avatar, Card, IconButton, Text} from 'react-native-paper';
+import usePetsStore from '../stores/pets';
+import {colors} from '../styles/colors';
+import useFeedbackStore from '../stores/feedback';
+import axios from '../axios.config';
+
+const {Title} = Card;
+const {Image} = Avatar;
+
+const styles = StyleSheet.create({
+  icon: {
+    margin: 0,
+  },
+  title: {
+    marginLeft: 50,
+  },
+  cardContent: {
+    margin: 20,
+    padding: 20,
+  },
+});
+
+type PetType = {
+  id: number;
+  name: string;
+  funLevel?: number;
+  life: number;
+};
+
+type Props = {
+  pets: PetType[];
+};
+
+const ListItem = ({id, name, funLevel, life}: PetType) => {
+  const {showMessage} = useFeedbackStore();
+  const {getPets} = usePetsStore();
+
+  const left = () => (
+    <Image size={80} source={require('../assets/daijin.png')} />
+  );
+
+  const right = () => (
+    <View>
+      <IconButton
+        style={styles.icon}
+        icon="pencil"
+        iconColor={colors.secondary}
+        onPress={() => handleEdit()}
+      />
+      <IconButton
+        style={styles.icon}
+        icon="delete"
+        iconColor={colors.error}
+        onPress={() => handleDelete()}
+      />
+    </View>
+  );
+
+  const handleEdit = () => {
+    console.log(id);
+  };
+
+  const handleDelete = () => {
+    Alert.alert('Alerta!', 'Tem certeza que deseja excluir esse pet?', [
+      {
+        text: 'Cancelar',
+        onPress: () => {
+          return;
+        },
+      },
+      {
+        text: 'Excluir',
+        onPress: async () => {
+          try {
+            await axios.delete(`/pet/${id}`);
+            showMessage({
+              type: 'success',
+              message: 'Pet deletado com sucesso',
+              visible: true,
+            });
+            getPets();
+          } catch (error) {
+            console.log(error);
+            showMessage({
+              type: 'success',
+              message:
+                'Houve um erro ao deletar pet, tente novamente mais tarde!',
+              visible: true,
+            });
+          }
+        },
+      },
+    ]);
+  };
+
+  return (
+    <Card mode="contained" style={styles.cardContent}>
+      <Title
+        title={name}
+        titleVariant="titleLarge"
+        subtitle={
+          <View>
+            <Text>Vida: {life}</Text>
+            <Text>Diversão: {funLevel}</Text>
+          </View>
+        }
+        subtitleNumberOfLines={2}
+        subtitleStyle={styles.title}
+        titleStyle={styles.title}
+        left={left}
+        right={right}
+      />
+    </Card>
+  );
+};
+
+const ListPets = ({pets}: Props) => {
+  return (
+    <FlatList
+      data={pets}
+      renderItem={({item}) => (
+        <ListItem
+          id={item.id}
+          name={item.name}
+          funLevel={item.funLevel}
+          life={item.life}
+        />
+      )}
+    />
+  );
+};
+
+export default ListPets;
--- src/screens/Home.tsx
+++ src/screens/Home.tsx
@@ -1,109 +1,14 @@
 import React, {useEffect} from 'react';
-import {FlatList, SafeAreaView, StyleSheet, View} from 'react-native';
-import {Avatar, Button, Card, IconButton, Text} from 'react-native-paper';
+import {SafeAreaView, StyleSheet} from 'react-native';
+import {Button} from 'react-native-paper';
 import usePetsStore from '../stores/pets';
-import {colors} from '../styles/colors';
-import axios from '../axios.config';
-import useFeedbackStore, {FeedbackMessage} from '../stores/feedback';
-
-const {Title} = Card;
-const {Image} = Avatar;
+import ListPets from '../components/ListPets';
 
 const styles = StyleSheet.create({
-  icon: {
-    margin: 0,
-  },
-  title: {
-    marginLeft: 50,
-  },
-  cardContent: {
-    margin: 20,
-    padding: 20,
-  },
   addButton: {
     margin: 20,
   },
 });
-
-type PetType = {
-  id: number;
-  name: string;
-  funLevel?: number;
-  life: number;
-};
-
-const ListItem = ({id, name, funLevel, life}: PetType) => {
-  const {showMessage} = useFeedbackStore();
-  const {getPets} = usePetsStore();
-
-  const left = () => (
-    <Image size={80} source={require('../assets/daijin.png')} />
-  );
-
-  const right = () => (
-    <View>
-      <IconButton
-        style={styles.icon}
-        icon="pencil"
-        iconColor={colors.secondary}
-        onPress={() => handleEdit(id)}
-      />
-      <IconButton
-        style={styles.icon}
-        icon="delete"
-        iconColor={colors.error}
-        onPress={() => handleDelete(id, showMessage, getPets)}
-      />
-    </View>
-  );
-
-  return (
-    <Card mode="contained" style={styles.cardContent}>
-      <Title
-        title={name}
-        titleVariant="titleLarge"
-        subtitle={
-          <View>
-            <Text>Vida: {life}</Text>
-            <Text>Diversão: {funLevel}</Text>
-          </View>
-        }
-        subtitleNumberOfLines={2}
-        subtitleStyle={styles.title}
-        titleStyle={styles.title}
-        left={left}
-        right={right}
-      />
-    </Card>
-  );
-};
-
-const handleEdit = (id: number) => {
-  console.log(id);
-};
-
-const handleDelete = async (
-  id: number,
-  showMessage: (state: FeedbackMessage) => void,
-  getPets: () => void,
-) => {
-  try {
-    await axios.delete(`/pet/${id}`);
-    showMessage({
-      type: 'success',
-      message: 'Pet deletado com sucesso',
-      visible: true,
-    });
-    getPets();
-  } catch (error) {
-    console.log(error);
-    showMessage({
-      type: 'success',
-      message: 'Houve um erro ao deletar pet, tente novamente mais tarde!',
-      visible: true,
-    });
-  }
-};
 
 const Home = ({navigation}: any) => {
   const {pets, getPets} = usePetsStore();
@@ -121,17 +26,7 @@ const Home = ({navigation}: any) => {
         onPress={() => navigation.navigate('Criar Pet')}>
         Adicionar
       </Button>
-      <FlatList
-        data={pets}
-        renderItem={({item}) => (
-          <ListItem
-            id={item.id}
-            name={item.name}
-            funLevel={item.funLevel}
-            life={item.life}
-          />
-        )}
-      />
+      <ListPets pets={pets} />
     </SafeAreaView>
   );
 };
