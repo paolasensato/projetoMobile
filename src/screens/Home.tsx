b551e4fdb4955c3aea015535681799e4e2211793
Paola Sensato Pasti <paolasensato03@gmail.com>
2023-10-31 21:23:04 -0300

    Adiciona botão de logout na home

--- src/screens/Home.tsx
+++ src/screens/Home.tsx
@@ -3,24 +3,38 @@ import {SafeAreaView, StyleSheet} from 'react-native';
 import {Button} from 'react-native-paper';
 import usePetsStore from '../stores/pets';
 import ListPets from '../components/ListPets';
+import useUserStore from '../stores/userStore';
 
 const styles = StyleSheet.create({
-  addButton: {
-    margin: 20,
+  button: {
+    marginHorizontal: 20,
+    marginVertical: 8,
   },
 });
 
 const Home = ({navigation}: any) => {
   const {pets, getPets} = usePetsStore();
+  const store = useUserStore();
 
   useEffect(() => {
     getPets();
   }, []);
 
+  const handleLogout = () => {
+    store.resetToken();
+  };
+
   return (
     <SafeAreaView>
       <Button
-        style={styles.addButton}
+        style={styles.button}
+        icon="logout"
+        mode="contained"
+        onPress={handleLogout}>
+        Sair
+      </Button>
+      <Button
+        style={styles.button}
         icon="plus"
         mode="contained"
         onPress={() => navigation.navigate('CreatePet')}>
--- src/stores/userStore.ts
+++ src/stores/userStore.ts
@@ -3,11 +3,13 @@ import {create} from 'zustand';
 type AuthState = {
   token: string | null;
   setToken: (token: string) => void;
+  resetToken: () => void;
 };
 
 const useUserStore = create<AuthState>(set => ({
   token: null,
   setToken: token => set(() => ({token})),
+  resetToken: () => set({token: null}),
 }));
 
 export default useUserStore;
